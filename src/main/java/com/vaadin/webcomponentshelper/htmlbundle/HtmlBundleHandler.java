587b1242fe48683542deb6e053e55d9097c0aaff
Artur Signell <artur@vaadin.com>
2017-05-02 11:51:15 +0300

    Rename org.vaadin -> com.vaadin to be able to deploy snapshots

--- src/main/java/com/vaadin/webcomponentshelper/htmlbundle/HtmlBundleHandler.java
+++ src/main/java/com/vaadin/webcomponentshelper/htmlbundle/HtmlBundleHandler.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.vaadin.webcomponentshelper.htmlbundle;
+package com.vaadin.webcomponentshelper.htmlbundle;
 
 import java.io.IOException;
 import java.net.URL;
--- src/main/java/com/vaadin/webcomponentshelper/polyfill/WebComponentsPolyfillMode.java
+++ src/main/java/com/vaadin/webcomponentshelper/polyfill/WebComponentsPolyfillMode.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.vaadin.webcomponentshelper.polyfill;
+package com.vaadin.webcomponentshelper.polyfill;
 
 /**
  * The mode for deciding whether to automatically load the webcomponentsjs
